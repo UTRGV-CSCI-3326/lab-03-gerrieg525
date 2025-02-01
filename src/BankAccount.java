4ed2ec86dcfefbc5d11850ffb4be902e5c76279f
Gerrie Gumban <122905264+gerrieg525@users.noreply.github.com>
2025-02-01 06:41:09 +0000

    Finished Lab 3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        double checkBalance = 2175.37;
+        checkBalance -= 302.50;
+        checkBalance += 50.03;
+        checkBalance /= 2;
+        checkBalance += 20.00;
+        checkBalance -= 1;
+        checkBalance *= 2;
+        checkBalance += 1;
+        System.out.printf("%.2f", checkBalance);
+        System.out.println();
+    }
 
+}
